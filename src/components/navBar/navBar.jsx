88172478f78c500f1d7c69c3d69d1b82338d4b3d
tursdlc <ana89.sdlc@gmail.com>
2024-09-01 19:05:52 +0200

    do navbar component

---
+++ src/components/navBar/navBar.jsx
@@ -0,0 +1,20 @@
+
+
+export default function Navbar(){
+    return(
+        <>
+        <div className="navbar">
+            <div className="pageLinks">
+                <p>Recipes</p>
+                <p>Add a recipe</p>
+            </div>
+            <div className="title">
+                <h1>Savor Street</h1>
+            </div>
+            <div className="search">
+                <input type="text" placeholder="Search for anything"></input>
+            </div>
+        </div>       
+        </>
+    )
+}
